bfde10dd89d96c2afc2074eb0cdaafcc30c228b5
Lars Ingebrigtsen <larsi@gnus.org>
2018-12-11 16:25:31 +0100

    Discogs now needs auth for searches

--- discogs.el
+++ discogs.el
@@ -16,23 +16,44 @@
 (require 'cl)
 (require 'json)
 
+(defvar discogs-consumer-key nil
+  "Get the key at discogs.com.")
+
+(defvar discogs-consumer-secret nil
+  "Get the secret at discogs.com.")
+
 (defun discogs-query (object identifier)
   (let ((url-request-extra-headers
 	 '(("User-Agent" . "discogs.el/1.0 +http://lars.ingebrigtsen.no/"))))
     (with-current-buffer
 	(url-retrieve-synchronously
-	 (format "http://api.discogs.com/%s/%s"
-		 object identifier))
+	 (format "https://api.discogs.com/%s/%s&key=%s&secret=%s"
+		 object identifier
+		 discogs-consumer-key
+		 discogs-consumer-secret))
       (goto-char (point-min))
       (prog1
 	  (when (re-search-forward "\r?\n\r?\n" nil t)
 	    (json-read))
 	(kill-buffer (current-buffer))))))
 
+(defun discogs-clean (string)
+  (replace-regexp-in-string "[?&-.,]" " " string))
+
 (defun discogs-search (artist title)
-  (discogs-query
-   "database" (format "search?type=master&artist=%s&release_title=%s"
-		      artist title)))
+  (setq artist (discogs-clean artist)
+	title (discogs-clean title))
+  (let ((results
+	 (discogs-query
+	  "database" (format "search?type=master&artist=%s&release_title=%s"
+			     artist title))))
+    (if (plusp (length (cdr (assoc 'results results))))
+	results
+      ;; Rate-limit to the API limit.
+      (sleep-for 1)
+      (discogs-query
+       "database" (format "search?type=release&q=%s %s"
+			  artist title)))))
 
 (defun discogs-find-year (artist title)
   (let ((data (discogs-search artist title)))
